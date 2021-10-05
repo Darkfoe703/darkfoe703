5df975238242e0ff51e8f2e44c509908346ee960
Marco Romero <marcoromero_1@hotmail.com>
2021-10-04 22:05:42 -0300

    Apply multilingual changes. Edit accessibility. Change colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,17 @@
 // SIDEBAR control
 // ==========================================================================
 function sb_open() {
-  let sideBar = document.getElementById("mySidebar");
+  const sideBar = document.getElementById("mySidebar");
   sideBar.classList.remove("animate-right", "sidebar-hide", "sidebar-init");
   sideBar.classList.add("animate-left", "sidebar-show");
 }
 function sb_close() {
-  let sideBar = document.getElementById("mySidebar");
+  const sideBar = document.getElementById("mySidebar");
   sideBar.classList.remove("animate-left", "sidebar-show");
   sideBar.classList.add("animate-right", "sidebar-hide");
 }
 
-var bbutton = document.getElementById("bbutton");
+const bbutton = document.getElementById("bbutton");
 bbutton.addEventListener("click", sb_open);
-var cbutton = document.getElementById("cbutton");
+const cbutton = document.getElementById("cbutton");
 cbutton.addEventListener("click", sb_close);
